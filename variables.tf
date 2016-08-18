d5e4a031626d2e31709c5fc9436ac7a68f4b08b4
Brent Langston <blangston@cloudpassage.com>
2016-08-18 17:50:13 -0400

    adding ecs-services

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "subnet_assignments" {
   type = "map"
   default = {
     brent           = "10.0.0.0/16"
+    ecs-services    = "10.1.0.0/16"
     cphalo          = "10.10.0.0/16"
     devops          = "10.11.0.0/16"
     akeller         = "10.50.0.0/16"
