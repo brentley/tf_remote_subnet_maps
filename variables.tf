08d89539f9c8dfccc0bdd052e717cb0cb96c07c4
Brent Langston <blangston@cloudpassage.com>
2016-08-19 19:33:20 -0400

    adding a cphalo stack

--- variables.tf
+++ variables.tf
@@ -15,6 +15,7 @@ variable "subnet_assignments" {
   default = {
     brent           = "10.0.0.0/16"
     ecs-services    = "10.1.0.0/16"
+    cphalo          = "10.8.0.0/16"
     loadtest        = "10.9.0.0/16"
     cphalo          = "10.10.0.0/16"
     devops          = "10.11.0.0/16"
