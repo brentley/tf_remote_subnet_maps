7c7e63cd0284ac6316169c52d60009bf6eadfea1
Brent Langston <blangston@cloudpassage.com>
2016-08-19 16:01:33 -0400

    updating grid name

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "subnet_assignments" {
   default = {
     brent           = "10.0.0.0/16"
     ecs-services    = "10.1.0.0/16"
-    load            = "10.9.0.0/16"
+    loadtest        = "10.9.0.0/16"
     cphalo          = "10.10.0.0/16"
     devops          = "10.11.0.0/16"
     akeller         = "10.50.0.0/16"
