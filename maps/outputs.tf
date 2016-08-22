79b87981833d8c6f34f5787016bac8e3dea82e27
Salim Kapadia <skapadia@cloudpassage.com>
2016-08-22 14:32:01 -0500

    adding additional entry

--- maps/outputs.tf
+++ maps/outputs.tf
@@ -5,56 +5,65 @@ output "computed_aws" {
 ###################
 
 output "computed_private_1" {
-	value = "${var.input_private_1}"	
+	value = "${var.input_private_1}"
 }
 output "computed_private_2" {
-	value = "${var.input_private_2}"	
+	value = "${var.input_private_2}"
 }
 output "computed_private_3" {
-	value = "${var.input_private_3}"	
+	value = "${var.input_private_3}"
 }
 output "computed_public_1" {
-	value = "${var.input_public_1}"	
+	value = "${var.input_public_1}"
 }
 output "computed_public_2" {
-	value = "${var.input_public_2}"	
+	value = "${var.input_public_2}"
 }
 output "computed_public_3" {
-	value = "${var.input_public_3}"	
+	value = "${var.input_public_3}"
 }
 output "computed_cache_1" {
-	value = "${var.input_cache_1}"		
+	value = "${var.input_cache_1}"
 }
 output "computed_cache_2" {
-	value = "${var.input_cache_2}"		
+	value = "${var.input_cache_2}"
 }
 output "computed_cache_3" {
-	value = "${var.input_cache_3}"		
+	value = "${var.input_cache_3}"
 }
 output "computed_db_1" {
-	value = "${var.input_db_1}"		
+	value = "${var.input_db_1}"
 }
 output "computed_db_2" {
-	value = "${var.input_db_2}"		
+	value = "${var.input_db_2}"
 }
 output "computed_db_3" {
-	value = "${var.input_db_3}"		
+	value = "${var.input_db_3}"
 }
 output "computed_cassandra_1" {
-	value = "${var.input_cassandra_1}"	
+	value = "${var.input_cassandra_1}"
 }
 output "computed_cassandra_2" {
-	value = "${var.input_cassandra_2}"	
+	value = "${var.input_cassandra_2}"
 }
 output "computed_cassandra_3" {
-	value = "${var.input_cassandra_3}"	
+	value = "${var.input_cassandra_3}"
 }
 output "computed_elasticsearch_1" {
-	value = "${var.input_elasticsearch_1}"	
+	value = "${var.input_elasticsearch_1}"
 }
 output "computed_elasticsearch_2" {
-	value = "${var.input_elasticsearch_2}"	
+	value = "${var.input_elasticsearch_2}"
 }
 output "computed_elasticsearch_3" {
-	value = "${var.input_elasticsearch_3}"	
+	value = "${var.input_elasticsearch_3}"
+}
+output "computed_master_1" {
+	value = "${var.input_master_1}"
+}
+output "computed_master_2" {
+	value = "${var.input_master_2}"
+}
+output "computed_master_3" {
+	value = "${var.input_master_3}"
 }
