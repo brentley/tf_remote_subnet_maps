770087d91a82262a2d155c7bb31f2f018104b571
Brent Langston <blangston@cloudpassage.com>
2016-08-24 14:43:19 -0400

    adding ehoffmann

--- variables.tf
+++ variables.tf
@@ -25,5 +25,6 @@ variable "subnet_assignments" {
     thor            = "10.53.0.0/16"
     badams          = "10.54.0.0/16"
     alim            = "10.55.0.0/16"
+    ehoffmann       = "10.56.0.0/16"
   }
 }
