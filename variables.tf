65d6765aa475587c547f7dc734c569b2cb5c81b3
Aliaksandr Buhayeu <abuhayeu@cloudpassage.com>
2016-10-04 16:09:24 +0300

    add abuhayeu subnet

--- variables.tf
+++ variables.tf
@@ -36,5 +36,6 @@ variable "subnet_assignments" {
     wfrancis        = "10.59.0.0/16"
     trobbins        = "10.60.0.0/16"
     aadriasola      = "10.61.0.0/16"
+    abuhayeu        = "10.62.0.0/16"
   }
 }
